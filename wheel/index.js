545534017802b450231d0af35880b5a5dbcfc767
Halla Moore <hallamoore7@gmail.com>
2021-10-27 21:56:14 -0400

    [wheel] rough pass at adding auto layout and player displays

--- wheel/index.js
+++ wheel/index.js
@@ -1,23 +1,159 @@
 const puzzles = [
   {
-    category: "phrase",
-    chars: [
-      ["_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_"],
-      ["_", "_", "_", "T", "H", "I", "S", "_", "I", "S", "_", "_", "_", "_"],
-      ["_", "_", "_", "_", "A", "_", "T", "E", "S", "T", "_", "_", "_", "_"],
-      ["_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_"]
-    ]
+    category: "before & after",
+    data: "zendesk ticket to ride"
+  },
+  {
+    category: "before & after",
+    data: "barcode read a book"
+  },
+  {
+    category: "best sellers",
+    data: "hpit torch with hsll"
+  },
+  {
+    category: "best sellers",
+    data: "dataman and insight"
+  },
+  {
+    category: "family",
+    data: "David and Jay Negro"
+  },
+  {
+    category: "occupation",
+    data: "Minister of Culture"
+  },
+  {
+    category: "occupation",
+    data: "Director of Products"
   },
   {
     category: "place",
-    chars: [
-      ["_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_"],
-      ["_", "_", "_", "B", "A", "C", "K", "Y", "A", "R", "D", "_", "_", "_"],
-      ["_", "_", "_", "_", "P", "A", "T", "I", "O", "_", "_", "_", "_", "_"],
-      ["_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_"]
-    ]
+    data: "Frisbee field"
+  },
+  {
+    category: "place",
+    data: "Aachen Germany"
+  },
+  {
+    category: "place",
+    data: "Cogercise Room"
+  },
+  {
+    category: "rhyme time",
+    data: "kite light at night"
+  },
+  {
+    category: "rhyme time",
+    data: "wren again"
+  },
+  {
+    category: "rhyme time",
+    data: "design a myna"
+  },
+  {
+    category: "rhyme time",
+    data: "eddie the yeti"
+  },
+  {
+    category: "values",
+    data: "sharing and integrity"
+  },
+  {
+    category: "values",
+    data: "creativity and enthusiasm"
+  },
+  {
+    category: "values",
+    data: "customer first and pride"
+  },
+  {
+    category: "what are you doing?",
+    data: "work hard"
+  },
+  {
+    category: "what are you doing?",
+    data: "play hard"
+  },
+  {
+    category: "what are you doing?",
+    data: "moving too fast"
   }
 ];
+
+const emptyShortRow = [];
+const emptyLongRow = [];
+for (let i = 0; i < 12; i++) {
+  emptyShortRow.push("_");
+  emptyLongRow.push("_");
+}
+for (let i = 0; i < 2; i++) {
+  emptyLongRow.push("_");
+}
+
+function createPuzzleLayout(input) {
+  const words = input.split(" ");
+  const rows = [];
+  let row = "";
+  for (let word of words) {
+    if (row.length + 1 + word.length < 12) {
+      row += " " + word;
+    } else {
+      const remainingChars = 12 - row.length;
+      const x = remainingChars / 2.0;
+      const a = Math.floor(x);
+      const b = Math.ceil(x);
+      row = row.padStart(row.length + a, " ");
+      row = row.padEnd(row.length + b, " ");
+      row = row.replaceAll(" ", "_");
+      rows.push(row.split(""));
+      row = word;
+    }
+  }
+
+  const remainingChars = 12 - row.length;
+  const x = remainingChars / 2.0;
+  const a = Math.floor(x);
+  const b = Math.ceil(x);
+  console.log(remainingChars, x, a, b);
+  row = row.padStart(row.length + a, " ");
+  row = row.padEnd(row.length + b, " ");
+  row = row.replaceAll(" ", "_");
+  rows.push(row.split(""));
+
+  console.log("a", rows);
+  switch (rows.length) {
+    case 1:
+      return [
+        emptyShortRow,
+        ["_", ...rows[0], "_"],
+        emptyLongRow,
+        emptyShortRow
+      ];
+      break;
+    case 2:
+      return [
+        emptyShortRow,
+        ["_", ...rows[0], "_"],
+        ["_", ...rows[1], "_"],
+        emptyShortRow
+      ];
+      break;
+    case 3:
+      return [
+        rows[0],
+        ["_", ...rows[1], "_"],
+        ["_", ...rows[2], "_"],
+        emptyLongRow
+      ];
+      break;
+    case 4:
+      return rows;
+      break;
+    default:
+      throw Error("More than 4 rows");
+  }
+}
 
 let cellsByChar = {};
 let currentPuzzleIndex = -1;
@@ -28,14 +164,16 @@ function nextPuzzle() {
     currentPuzzleIndex = 0;
   }
   const puzzle = puzzles[currentPuzzleIndex];
+  const puzzleChars = createPuzzleLayout(puzzle.data);
+  console.log(puzzleChars);
 
   document.querySelector("#category").innerText = puzzle.category.toUpperCase();
   cellsByChar = {};
   const rows = document.querySelectorAll("tr");
 
   for (let i = 0; i < rows.length; i++) {
-    if (puzzle.chars.length <= i) break;
-    const puzzleRow = puzzle.chars[i];
+    if (puzzleChars.length <= i) break;
+    const puzzleRow = puzzleChars[i];
     const cells = rows[i].querySelectorAll("td");
 
     for (let j = 0; j < cells.length; j++) {
