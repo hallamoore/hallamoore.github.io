bf2b98122ed31f9af5282ec72ef93ca928c008eb
Halla Moore <hallamoore7@gmail.com>
2021-10-28 13:24:35 -0400

    [wheel] Add money tracking

---
+++ wheel/money.js
@@ -0,0 +1,172 @@
+let currentPlayer = 1;
+let currentMoneyPerLetter = 0;
+const moneyByPlayerId = {};
+
+function rewardMoney(numLettersRevealed) {
+  moneyByPlayerId[currentPlayer].incrementBy(
+    currentMoneyPerLetter * numLettersRevealed
+  );
+}
+
+function deductVowelCost() {
+  moneyByPlayerId[currentPlayer].incrementBy(-250);
+}
+
+class FlipDigit {
+  constructor() {
+    this.node = document.createElement("div");
+    this.node.classList.add("flip-digit");
+
+    for (let i = 0; i < 10; i++) {
+      this.appendDigit(i);
+    }
+    // Add another 0 at the end to allow for smoother visuals when the flip animation wraps back
+    // around to the beginning
+    this.appendDigit(0);
+
+    this._height = 0;
+  }
+
+  get height() {
+    if (this._height) return this._height;
+    this._height = this.node.clientHeight;
+    return this._height;
+  }
+
+  appendDigit(i) {
+    const digitElem = document.createElement("div");
+    digitElem.classList.add("number-card");
+    digitElem.innerText = i;
+    this.node.appendChild(digitElem);
+  }
+
+  flipBy(delta) {
+    const totalScrollBy = this.height * delta;
+    const startOfSecond0 = this.height * 10;
+
+    let finalScrollTop = this.node.scrollTop + totalScrollBy;
+    while (finalScrollTop < 0) {
+      finalScrollTop += this.height * 10;
+    }
+    finalScrollTop = finalScrollTop % (this.height * 10);
+
+    const desiredDuration = 500;
+    const durationBetweenIntervals = 100;
+    const numIntervals = desiredDuration / durationBetweenIntervals;
+    // Round to prevent weird float precision behavior
+    const roundFn = delta > 0 ? Math.ceil : Math.floor;
+    const scrollByPerInterval = roundFn(totalScrollBy / numIntervals);
+
+    const scrollTopWrapCondition = delta > 0 ? startOfSecond0 : 0;
+    const scrollToWhenWrapping = delta > 0 ? 0 : startOfSecond0;
+
+    let scrolledSoFar = 0;
+    const intervalId = setInterval(() => {
+      this.node.scrollBy(0, scrollByPerInterval);
+      scrolledSoFar += scrollByPerInterval;
+
+      if (this.node.scrollTop === scrollTopWrapCondition) {
+        this.node.scrollTo(0, scrollToWhenWrapping);
+      }
+
+      if (Math.abs(scrolledSoFar) >= Math.abs(totalScrollBy)) {
+        // Adjust for any surplus scroll caused by scrollByPerInterval being rounded
+        this.node.scrollTo(0, finalScrollTop);
+        clearInterval(intervalId);
+      }
+    }, durationBetweenIntervals);
+  }
+
+  flipForwardTo(target) {
+    let delta = target - this.value();
+    if (delta < 0) {
+      delta += 10;
+    }
+    this.flipBy(delta);
+  }
+
+  flipBackwardsTo(target) {
+    let delta = this.value() - target;
+    if (delta < 0) {
+      delta += 10;
+    }
+    this.flipBy(-delta);
+  }
+
+  value() {
+    // Mod 10 to account for if we're at the second 0. That should be considered 0, not 10.
+    return (this.node.scrollTop / this.height) % 10;
+  }
+}
+
+class Money {
+  constructor(playerId) {
+    this.node = document.createElement("div");
+    this.node.classList.add("money");
+    this.node.innerText = "$";
+    document.querySelector(`#player${playerId}`).appendChild(this.node);
+
+    this.flipDigits = [];
+    this.prependFlipDigit();
+  }
+
+  prependFlipDigit() {
+    const flipDigit = new FlipDigit();
+    this.flipDigits.unshift(flipDigit);
+    if (this.node.children.length === 0) {
+      this.node.appendChild(flipDigit.node);
+    } else {
+      this.node.insertBefore(flipDigit.node, this.node.children[0]);
+    }
+    // Prevent scroll state on refresh
+    flipDigit.node.scrollTo(0, 0);
+
+    return flipDigit;
+  }
+
+  popFlipDigit() {
+    const flipDigit = this.flipDigits.shift();
+    flipDigit.node.remove();
+  }
+
+  value() {
+    let value;
+    for (let digit of this.flipDigits) {
+      if (value == null) {
+        value = digit.value();
+      } else {
+        value *= 10;
+        value += digit.value();
+      }
+    }
+    return value;
+  }
+
+  incrementBy(incr) {
+    const previousValue = this.value();
+    const nextValue = previousValue + incr;
+    const nextValueDigits = nextValue
+      .toString()
+      .split("")
+      .map(d => parseInt(d));
+    const numDigitDiff =
+      nextValueDigits.length - previousValue.toString().length;
+
+    if (incr > 0) {
+      for (let i = 0; i < numDigitDiff; i++) {
+        this.prependFlipDigit();
+      }
+      for (let i = 0; i < nextValueDigits.length; i++) {
+        this.flipDigits[i].flipForwardTo(nextValueDigits[i]);
+      }
+    } else {
+      for (let i = numDigitDiff; i < 0; i++) {
+        this.popFlipDigit();
+      }
+
+      for (let i = 0; i < nextValueDigits.length; i++) {
+        this.flipDigits[i].flipBackwardsTo(nextValueDigits[i]);
+      }
+    }
+  }
+}
---
+++ wheel/puzzle_operations.js
@@ -0,0 +1,105 @@
+let cellsByChar = {};
+let currentPuzzleIndex = -1;
+
+function nextPuzzle() {
+  currentPuzzleIndex++;
+  if (currentPuzzleIndex >= puzzles.length) {
+    currentPuzzleIndex = 0;
+  }
+
+  const puzzle = puzzles[currentPuzzleIndex];
+  const puzzleLayout = createPuzzleLayout(puzzle.data);
+
+  document.querySelector("#category").innerText = puzzle.category.toUpperCase();
+
+  cellsByChar = {};
+  const rows = document.querySelectorAll("tr");
+  for (let i = 0; i < rows.length; i++) {
+    const puzzleRow = puzzleLayout[i];
+    const cells = rows[i].querySelectorAll("td");
+
+    for (let j = 0; j < cells.length; j++) {
+      const cell = cells[j];
+      cell.onclick = null;
+
+      if (cell.childNodes.length == 2) {
+        // This cell had text appended to it as part of the last puzzle. Remove that text.
+        cell.childNodes[1].remove();
+      }
+
+      const img = cell.querySelector("img");
+      const char = puzzleRow[j].toUpperCase();
+      if (char === "_") {
+        img.src = "unused_cell.png";
+      } else {
+        if (!cellsByChar[char]) {
+          cellsByChar[char] = [];
+        }
+        cellsByChar[char].push(cell);
+        img.src = "";
+      }
+    }
+  }
+}
+
+function createPuzzleLayout(input) {
+  let rows = justify(input, 12);
+
+  if (rows.length === 1) {
+    const words = rows[0].split(" ");
+    if (words.length > 1) {
+      // Let's split this up into two rows
+      const numChars = rows[0].replaceAll(" ", "").length;
+      const maxWordLength = Math.max(...words.map(word => word.length));
+      const shorterLineLength = Math.max(
+        Math.ceil(numChars / 2),
+        maxWordLength
+      );
+      rows = justify(input, shorterLineLength);
+    }
+  }
+
+  const maxLength = Math.max(...rows.map(r => r.length));
+  const numStartPadding = Math.ceil((14 - maxLength) / 2);
+
+  rows = rows.map(row =>
+    row
+      .padStart(row.length + numStartPadding, "_")
+      .padEnd(14, "_")
+      .replaceAll(" ", "_")
+      .split("")
+  );
+
+  const emptyRow = repeatArray(14, "_");
+  return padStartAndEnd(rows, 4, emptyRow, { favorStartPadding: false });
+}
+
+function guessLetter(char) {
+  char = char.toUpperCase();
+  if (!cellsByChar[char]) return;
+
+  if (["A", "E", "I", "O", "U"].includes(char)) {
+    deductVowelCost();
+  } else {
+    rewardMoney(cellsByChar[char].length);
+  }
+
+  for (let cell of cellsByChar[char]) {
+    cell.classList.add("highlighted");
+    cell.onclick = () => {
+      cell.append(char);
+      cell.classList.remove("highlighted");
+      cell.onclick = null;
+    };
+  }
+  delete cellsByChar[char];
+}
+
+function solvePuzzle() {
+  for (let [char, cells] of Object.entries(cellsByChar)) {
+    for (let cell of cells) {
+      cell.append(char);
+    }
+  }
+  cellsByChar = {};
+}
